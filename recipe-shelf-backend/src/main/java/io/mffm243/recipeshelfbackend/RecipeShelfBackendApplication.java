d96d4147423e7545b30cc2d46e13b46a2e8b6991
fraser.macinnes <fraser.macinnes@deepmatter.io>
2022-03-12 14:43:52 +0000

    add backend project skeleton

---
+++ recipe-shelf-backend/src/main/java/io/mffm243/recipeshelfbackend/RecipeShelfBackendApplication.java
@@ -0,0 +1,13 @@
+package io.mffm243.recipeshelfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeShelfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeShelfBackendApplication.class, args);
+	}
+
+}
---
+++ recipe-shelf-backend/src/test/java/io/mffm243/recipeshelfbackend/RecipeShelfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mffm243.recipeshelfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeShelfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
